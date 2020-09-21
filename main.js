e18f4dc072ec108bc1d22bd15c59c50ffc1c4720
DANIEL JAMES <daniel.james@codeimmersives.com>
2020-09-21 13:40:59 -0400

    completed Promise #1

--- main.js
+++ main.js
@@ -1,27 +1,51 @@
 // 1.
 // Create a promise called myFirstPromise
+const myPromise = new Promise( (resolve, reject)=>{
+  // Inside the promise
+  // Create a boolean variable check and set it to true
+  let booleanValue = true
 
-// Inside the promise
-// Create a boolean variable check and set it to true
-// Create a variable rand and it should calculate a random number between 1 and 10
+  // Create a variable rand and it should calculate a random number between 1 and 10
+  let rand = Math.floor((Math.random() * 10) +1);
 
+  // If the check boolean is true it should resolve with the
+  // result of a random number between one and ten
+  if(booleanValue){
+    setTimeout( ()=> {
 
-// If the check boolean is true it should resolve with the
-// result of a random number between one and ten
+      resolve(rand);
+    },2000)
+  }
+  // if the check boolean is false, it should reject with
+  // a string that says: Cannot computer random number
+  else {
+    setTimeout( ()=>{
 
-// if the check boolean is false, it should reject with
-// a string that says: Cannot computer random number
-
-// Both resolve and reject should only occur after 2 seconds
+      reject('cannot computer random number')
+    }, 2000);
+  }
+  // Both resolve and reject should only occur after 2 seconds
+  
+}
+);
 
 // Now call the promise you created and chain your thenables.
-// In your first thenable
-// console.log('I have my random number <the number> and I will multiply it by 5')
-// Multiply your random number by 5 and pass the data to the next thenable
+myPromise.then((rand)=>{
+  // In your first thenable
+  // console.log('I have my random number <the number> and I will multiply it by 5')
+  // Multiply your random number by 5 and pass the data to the next thenable
+  console.log(`i have my random number ${rand} and i will multiply it by 5`)
+  let result = rand * 5
+  return result
+})
 
 // In your second thenable log a String
 // that says `Here is the result of my random number multiplied
 // by 5: < place number result here>`
+.then((result)=>{
+  console.log(`Here is the result of my random number multiplied by 5: ${result}`)
+
+})
 
 
 
@@ -44,3 +68,28 @@ let data = [
   { firstName: 'Doug', lastName: 'Lawson' },
   { firstName: 'Sandra', lastName: 'Mathers' },
 ];
+
+
+let getDataPromise = ()=>{
+  
+  return new Promise( (resolve, reject)=>{
+    let error = false;
+    if(error){
+      reject(`something went wrong`)
+    }
+    else {
+      setTimeout( ()=>{
+        resolve(data)
+      }, 4000)
+    }
+    
+
+  })
+};
+
+getDataPromise.then((data)=>{
+
+})
+
+
+
